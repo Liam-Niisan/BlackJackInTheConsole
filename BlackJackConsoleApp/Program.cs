6557434e3cb02d4adf36ba6b40f5de93c2194ee2
Liam_Niisan <robbie.barham@gmail.com>
2021-04-16 14:31:11 -0400

    This commit: Adds functions to calculate value of a hand and the hit functions.

--- BlackJackConsoleApp/Program.cs
+++ BlackJackConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BlackJackConsoleApp
 {
@@ -9,6 +10,9 @@ namespace BlackJackConsoleApp
         {
             
         }
+
+        //GAME STATES
+        public enum GameResult { Win = 1, Lose = -1, Draw = 0, Pending = 2};
 
         public class Card
         { 
@@ -98,7 +102,51 @@ namespace BlackJackConsoleApp
                     return d;
                 }
             }
+            //returns a shuffled deck
+            public static Deck ShuffleDeck
+            {
+                get 
+                {
+                    return new Deck(NewDeck.OrderBy(Card => System.Guid.NewGuid()).ToArray());
+                }
+            }
 
+            // calculate the value of a hand.
+            // A Hand is just a few cards so we can represent as Deck<Card> again.
+            // I compare two totals for aces and return the one closest to "less than or equal to 21".
+            public static double HandValue(Deck deck)
+            {
+                //Ace = 1
+                int val1 = deck.Sum(c => c.Value);
+
+                //Ace = 11
+                double aces = deck.Count(c => c.Suit == "A");
+                double val2 = aces > 0 ? val1 + (10 * aces) : val1;
+
+                return new double[] { val1, val2 }
+                    .Select(handVal => new { handVal, weight = Math.Abs(handVal - 21) + (handVal > 21 ? 100 : 0)})
+                    .OrderBy(n => n.weight)
+                    .First().handVal;
+            }
+
+            // a few more rules
+            // check if dealer can hit given current value of Hand. Assume Dealer will always stand on 17 and 
+            // not hit on soft 17. Hence standLimit.
+            public static bool CanDealerHit(Deck deck, int standLimit)
+            {
+                return deck.Value < standLimit;
+            }
+
+            // No point hitting above 21...
+            public static bool CanPlayerHit(Deck deck)
+            {
+                return deck.Value < 21;
+            }
+
+            public static GameResult GetResult(Member player, Member dealer)
+            { 
+                
+            }
         }
     }
 }
